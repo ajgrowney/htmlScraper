67d81b7d3e9e2fcb52ba4c4f3f49174d550d3680
Andrew Growney <ajgrowney@ku.edu>
2018-08-26 10:15:00 -0500

    New: Abstracting Tag function

--- main.cpp
+++ main.cpp
@@ -6,63 +6,67 @@
 #include "HtmlTag.hpp"
 using namespace std;
 
+void parseTag(HtmlDoc* scrapeDoc, string line_in){
+    line_in.erase(0,1); // Get rid of '<'
+
+    // Determine if Opening or Closing tag
+    bool isOpeningTag = (line_in.at(0) == '/') ? false : true;
+
+    //-------------Case: Opening Tag-------------------
+    if(isOpeningTag){
+        cout << "Found Opening Tag: " << line_in.substr(0, line_in.find('>')) << "\n";
+        
+        // Find first delimiter
+        int delim_loc = line_in.find_first_of(" >");
+
+        // Create an object for the opening tag for mapping
+        string tagName = line_in.substr(0,delim_loc);
+        HtmlTag* newHtmlTag = new HtmlTag(tagName);
+
+        while(delim_loc != -1){
+            line_in.erase(0,delim_loc); // Erase tag name up to delimiter
+            if(line_in.at(0) == '>'){
+                // End of Opening Tag: '>'
+                line_in.erase(0,1); //Erase the delimiter
+            }else{
+                // Found Space: contains attribute
+                line_in.erase(0,1); // Erase the space
+
+                string attrName, attrVal;
+                int endAttrName = line_in.find_first_of(" =");
+                attrName = line_in.substr(0,endAttrName);
+                line_in.erase(0,endAttrName);
+                int beginAttrVal = line_in.find_first_of('"');
+                line_in.erase(0,beginAttrVal+1);
+                int endAttrVal = line_in.find_first_of('"');
+                attrVal = line_in.substr(0,endAttrVal);
+
+                newHtmlTag->insertAttribute(attrName, attrVal);
+            }
+            delim_loc = line_in.find_first_of(" >");
+        }
+
+        scrapeDoc->insertDocumentTag(tagName, newHtmlTag);
+
+    }else{
+        //---------Case: Closing Tag---------------------
+        line_in.erase(0,1); // Erase '/'
+        cout << "Found Closing Tag: " << line_in.substr(0, line_in.find('>')) << "\n";
+        // Found a closing tag
+
+    }
+}
 
 // Assuming the line begins with a '<' 
 void parseLine(HtmlDoc* scrapeDoc, string line_in){
     scrapeDoc->addLine();
 
-    int tag_loc = line_in.find_first_of("<");
-    line_in.erase(0,tag_loc);
+    int end_whitesapce = line_in.find_first_not_of(" \t");
+    line_in.erase(0,end_whitesapce);
 
-    if(tag_loc != -1){
-        line_in.erase(0,1); // Get rid of '<'
-
-        // Determine if Opening or Closing tag
-        bool isOpeningTag = (line_in.at(0) == '/') ? false : true;
-
-        //-------------Case: Opening Tag-------------------
-        if(isOpeningTag){
-            cout << "Found Opening Tag: " << line_in << "\n";
-            
-            // Find first delimiter
-            int delim_loc = line_in.find_first_of(" >");
-
-            // Create an object for the opening tag for mapping
-            string tagName = line_in.substr(0,delim_loc);
-            HtmlTag* newHtmlTag = new HtmlTag(tagName);
-
-            while(delim_loc != -1){
-                line_in.erase(0,delim_loc); // Erase tag name up to delimiter
-                if(line_in.at(0) == '>'){
-                    // End of Opening Tag: '>'
-                    line_in.erase(0,1); //Erase the delimiter
-                }else{
-                    // Found Space: contains attribute
-                    line_in.erase(0,1); // Erase the space
-
-                    string attrName, attrVal;
-                    int endAttrName = line_in.find_first_of(" =");
-                    attrName = line_in.substr(0,endAttrName);
-                    line_in.erase(0,endAttrName);
-                    int beginAttrVal = line_in.find_first_of('"');
-                    line_in.erase(0,beginAttrVal+1);
-                    int endAttrVal = line_in.find_first_of('"');
-                    attrVal = line_in.substr(0,endAttrVal);
-
-                    newHtmlTag->insertAttribute(attrName, attrVal);
-                }
-                delim_loc = line_in.find_first_of(" >");
-            }
-
-            scrapeDoc->insertDocumentTag(tagName, newHtmlTag);
-
-        }else{
-            //---------Case: Closing Tag---------------------
-            line_in.erase(0,1); // Erase '/'
-            cout << "Found Closing Tag: " << line_in << "\n";
-            // Found a closing tag
-
-        }
+    // Found the opening '<' for a tag
+    if(end_whitesapce != -1 && line_in.at(0) == '<'){
+        parseTag(scrapeDoc, line_in);
     }
 }
 
